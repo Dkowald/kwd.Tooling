06c73b0a0d46dbfeec1d24c10cd904dd138c0b7b
dkowald <derek@kowald.id.au>
2023-08-24 11:29:37 +1000

    Add razorpart and fluxpart templates.

---
+++ src/Samples/Feature/flux/Store/MyPartState.cs
@@ -0,0 +1,11 @@
+﻿using Fluxor;
+
+namespace Samples.Feature.flux.Store;
+
+[FeatureState]
+public record MyPartState
+{
+  [ReducerMethod]
+  public static MyPartState OnMyClickAction(MyPartState state /*, MyClickAction action*/)
+    => state with { };
+}
---
+++ src/kwd.Templates/RazorPart/SampleRazorComponent.razor.cs
@@ -0,0 +1,8 @@
+﻿//todo: move this to the namespace that matches the razor component.
+namespace kwd.Templates.RazorPart;
+
+/// <summary>
+/// My SampleRazorComponent
+/// </summary>
+public partial class SampleRazorComponent{}
+
